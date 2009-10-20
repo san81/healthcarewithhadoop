6199b3361785ab47be195d21d2ebf9c2279be70c
nitin.tripathi <nitin.tripathi@f0b12dc0-bd3b-11de-8784-f7ad4773dbab>
2009-10-20 13:12:59 +0000

    refactored changes to parser.

--- trunk/healthcare/src/com/pramati/healthcare/core/tools/TableConfigurationParser.java
+++ trunk/healthcare/src/com/pramati/healthcare/core/tools/TableConfigurationParser.java
@@ -1,13 +1,16 @@
 package com.pramati.healthcare.core.tools;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.log4j.Logger;
 
 /**
  * 
@@ -34,6 +37,9 @@ public class TableConfigurationParser {
 	public List<Table> parse(String fileName) throws Exception {
 		XMLConfiguration configuration = getConfiguration(fileName);
 		List<Table> tables = configuration.getList(TABLE_NAME);
+		if (CollectionUtils.isEmpty(tables)) {
+			throw new IllegalStateException();
+		}
 		HierarchicalConfiguration table = null;
 		for (int size = tables.size(), index = 0; index < size; index++) {
 			table = configuration
@@ -51,6 +57,10 @@ public class TableConfigurationParser {
 	 */
 	private XMLConfiguration getConfiguration(String fileName)
 			throws ConfigurationException {
+		File file = new File(fileName);
+		if (!file.exists()) {
+			throw new IllegalArgumentException();
+		}
 		XMLConfiguration configuration = new XMLConfiguration();
 		configuration.setFileName(fileName);
 		configuration.setValidating(VALIDATE);
