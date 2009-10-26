98bdf7c77677e2736b4d29f06579732ac5b52e7e
nitin.tripathi <nitin.tripathi@f0b12dc0-bd3b-11de-8784-f7ad4773dbab>
2009-10-26 06:29:31 +0000

    Added following methods to Problem: 1. Override equals, hashcode method 2. Implemented Comparable interface

--- trunk/healthcare/src/com/pramati/healthcare/model/Problem.java
+++ trunk/healthcare/src/com/pramati/healthcare/model/Problem.java
@@ -2,7 +2,7 @@ package com.pramati.healthcare.model;
 
 import java.util.Date;
 
-public class Problem {
+public class Problem implements Comparable<Problem> {
 	private String description;
 	private Date startDate;
 	private Episodicity episodicity;
@@ -56,6 +56,79 @@ public class Problem {
 
 	public void setOnset(Onset onset) {
 		this.onset = onset;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Problem other = (Problem) obj;
+		if (course == null) {
+			if (other.course != null)
+				return false;
+		} else if (!course.equals(other.course))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (episodicity == null) {
+			if (other.episodicity != null)
+				return false;
+		} else if (!episodicity.equals(other.episodicity))
+			return false;
+		if (onset == null) {
+			if (other.onset != null)
+				return false;
+		} else if (!onset.equals(other.onset))
+			return false;
+		if (severity == null) {
+			if (other.severity != null)
+				return false;
+		} else if (!severity.equals(other.severity))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((course == null) ? 0 : course.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result
+				+ ((episodicity == null) ? 0 : episodicity.hashCode());
+		result = prime * result + ((onset == null) ? 0 : onset.hashCode());
+		result = prime * result
+				+ ((severity == null) ? 0 : severity.hashCode());
+		result = prime * result
+				+ ((startDate == null) ? 0 : startDate.hashCode());
+		return result;
+	}
+
+	/**
+	 * sorts in reverse order by start date, that is, latest first.
+	 */
+	@Override
+	public int compareTo(Problem other) {
+		return -1 * (this.getStartDate().compareTo(other.getStartDate()));
+	}
+
+	@Override
+	public String toString() {
+		return "Problem [description=" + description + ", startDate="
+				+ startDate + "]";
 	}
 
 }
