0ca14c8ffa74dc2f27db87323cb594b016444099
santhosh.gandhe <santhosh.gandhe@f0b12dc0-bd3b-11de-8784-f7ad4773dbab>
2009-10-26 13:40:35 +0000

    Changed to a webapp and spring mvc added

---
+++ trunk/healthcare/src/com/pramati/healthcare/web/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.pramati.healthcare.web.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class LoginController implements Controller {
+
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String aMessage = "Hello World MVC!";
+		
+		ModelAndView modelAndView = new ModelAndView("hello_world");
+		modelAndView.addObject("message", aMessage);
+		
+		return modelAndView;
+	}
+}
