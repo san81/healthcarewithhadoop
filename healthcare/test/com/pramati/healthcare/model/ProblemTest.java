b9a1c5266830da2be762e93faa3938c8c0c7fe79
nitin.tripathi <nitin.tripathi@f0b12dc0-bd3b-11de-8784-f7ad4773dbab>
2009-10-26 06:29:31 +0000

    Added following methods to Problem: 1. Override equals, hashcode method 2. Implemented Comparable interface

---
+++ healthcare/test/com/pramati/healthcare/model/ProblemTest.java
@@ -0,0 +1,41 @@
+package com.pramati.healthcare.model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class ProblemTest extends TestCase {
+	@Test
+	public void testEquals() throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Problem problem = new Problem();
+		problem.setStartDate(dateFormat.parse("26/10/2009"));
+		Problem anotherProblem = new Problem();
+		anotherProblem.setStartDate(dateFormat.parse("26/10/2009"));
+		assertEquals(false, problem.equals(null));
+		assertEquals(true, problem.equals(problem));
+		assertEquals(true, problem.equals(anotherProblem));
+	}
+
+	public void testCompareTo() throws ParseException {
+		Problem problem = new Problem();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		problem.setStartDate(dateFormat.parse("26/10/2009"));
+		Problem problem2 = new Problem();
+		problem2.setStartDate(dateFormat.parse("25/10/2009"));
+		Problem problem3 = new Problem();
+		problem3.setStartDate(dateFormat.parse("27/10/2009"));
+		List<Problem> problemList = new ArrayList<Problem>();
+		problemList.add(problem);
+		problemList.add(problem2);
+		problemList.add(problem3);
+		Collections.sort(problemList);
+		assertEquals(problem3, problemList.get(0));
+	}
+}
